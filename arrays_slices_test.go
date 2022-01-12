bd7e146e60e22bf626ef40002f0e1473a0ec404b
Houdini <nonsoamadi@aol.com>
2022-01-12 12:49:26 +0100

    :zap: refactor test case

--- arrays_slices_test.go
+++ arrays_slices_test.go
@@ -28,35 +28,47 @@ func TestSum(t *testing.T){
 
 func TestSumAll( t *testing.T) {
 	
+	checkSums := func(t testing.TB, got, want []int) {
+        t.Helper()
+        if !reflect.DeepEqual(got, want) {
+            t.Errorf("got %v want %v", got, want)
+        }
+    }
 
 	t.Run("It should sum all slices", func(t *testing.T) {
 		got := SumAll([]int{1, 2}, []int{0, 9})
     	want := []int{3, 9}
 		
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %v want %v", got, want)
-		}
+		checkSums(t, got, want)
 	})
 }
 
 func TestSumAllTails(t *testing.T) {
 
+	checkSums := func(t testing.TB, got, want []int) {
+        t.Helper()
+        if !reflect.DeepEqual(got, want) {
+            t.Errorf("got %v want %v", got, want)
+        }
+    }
 	t.Run("It should sum all tails of a slice", func(t *testing.T) {
 		got := SumAllTails([]int{1, 2}, []int{0, 9})
     	want := []int{2, 9}
 
-    	if !reflect.DeepEqual(got, want) {
-        t.Errorf("got %v want %v", got, want)
-    }
+    	checkSums(t, got, want)
 	})
 
 	t.Run("safely sum empty slices", func(t *testing.T) {
+		checkSums := func(t testing.TB, got, want []int) {
+			t.Helper()
+			if !reflect.DeepEqual(got, want) {
+				t.Errorf("got %v want %v", got, want)
+			}
+		}
         got := SumAllTails([]int{}, []int{3, 4, 5})
         want := []int{0, 9}
 
-        if !reflect.DeepEqual(got, want) {
-            t.Errorf("got %v want %v", got, want)
-        }
+        checkSums(t, got, want)
     })
     
 }
