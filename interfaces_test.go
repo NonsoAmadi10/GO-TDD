ab2e85eb828a3d3700053fb14cb54ecfee00921a
Houdini <nonsoamadi@aol.com>
2022-01-13 17:35:06 +0100

    :zap: refactor test to accomodate methods

--- interfaces_test.go
+++ interfaces_test.go
@@ -14,25 +14,27 @@ func TestPerimeter(t *testing.T){
 	})
 }
 
-func TestArea(t *testing.T){
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{3.0, 6.0}
-		got := rectangle.Area()
-		want := 18.0
+func TestArea(t *testing.T) {
 
-		if got != want {
-        t.Errorf("got %.2f want %.2f", got, want)
-    	}
-	})
+    areaTests := []struct {
+        name    string
+        shape   Shape
+        hasArea float64
+    }{
+        {name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, hasArea: 72.0},
+        {name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+        {name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
+    }
 
-	t.Run("Circles", func(t *testing.T) {
-		circle := Circle{10.0}
+    for _, tt := range areaTests {
+        // using tt.name from the case to use it as the `t.Run` test name
+        t.Run(tt.name, func(t *testing.T) {
+            got := tt.shape.Area()
+            if got != tt.hasArea {
+                t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+            }
+        })
 
-		got := circle.Area()
-		want := 314.1592653589793
+    }
 
-		if got != want {
-        t.Errorf("got %.2f want %.2f", got, want)
-    	}
-	})
 }
