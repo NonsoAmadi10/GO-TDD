ae084d0f0c765199532cfdd40308ae65ca72a597
Houdini <nonsoamadi@aol.com>
2022-01-13 20:26:38 +0100

    :hammer: refactor function to use descriptive type

--- pointers_test.go
+++ pointers_test.go
@@ -1,15 +1,20 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestWallet(t *testing.T){
 
 	wallet := Wallet{}
 
-	wallet.Deposit(10)
+	wallet.Deposit(Bitcoin(10))
 
 	got:= wallet.Balance()
-	want := 10 
+	want := Bitcoin(10)
+
+	
+
 
 	if got != want {
 		t.Errorf("got %d but wanted %d", got, want)
